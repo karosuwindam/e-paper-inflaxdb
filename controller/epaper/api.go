1ef0a0f4626cb764ca51c10d22c61e4ee433888e
karosuwindam <karosuwindam@gmail.com>
2024-12-03 11:39:52 +0900

    update

--- controller/epaper/api.go
+++ controller/epaper/api.go
@@ -15,8 +15,11 @@ func Init() (*api, error) {
 	return &api{}, nil
 }
 
-func (a *api) TextPut(ctx context.Context, x, y int, text []string, size float64) {
-	testPut(ctx, x, y, text, size)
+func (a *api) TextPut(ctx context.Context, x, y int, texts []string, size float64) {
+	ctx, span := config.TracerS(ctx, "epaper.TextPut", "epaper")
+	defer span.End()
+	slog.DebugContext(ctx, "testPut", "x", x, "y", y, "text", texts, "size", size)
+	testPut(ctx, x, y, texts, size)
 }
 
 func (a *api) ClearScreen(ctx context.Context) {
