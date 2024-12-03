c503417ed03da27e633aedbc6551d7458becc9e6
karosuwindam <karosuwindam@gmail.com>
2024-12-03 11:45:33 +0900

    update

--- controller/getInflux/api.go
+++ controller/getInflux/api.go
@@ -2,6 +2,8 @@ package getinflux
 
 import (
 	"context"
+	"epaperifdb/config"
+	"log/slog"
 	"time"
 )
 
@@ -19,9 +21,15 @@ func Init(url, db, table string) (*api, error) {
 }
 
 func (a *api) InfluxdbDay(ctx context.Context, dayago int, datatype string) DataFormat {
+	ctx, span := config.TracerS(ctx, "Influxdb.Day", "influxdb")
+	defer span.End()
+	slog.DebugContext(ctx, "getInfluxdbData", "dayago", dayago, "datatype", datatype)
 	return a.influx.getInfluxdbData(ctx, dayago, datatype)
 }
 
 func (a *api) InfluxdbBack(ctx context.Context, backdate time.Duration, datatype string) DataFormat {
+	ctx, span := config.TracerS(ctx, "Influxdb.Day", "influxdb")
+	defer span.End()
+	slog.DebugContext(ctx, "getInfluxdbData", "backdate", backdate, "datatype", datatype)
 	return a.influx.getInfluxdbData(ctx, backdate, datatype)
 }
