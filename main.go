d186f12f5397c65899787942d8f4f62b9ad1d754
karosuwindam <karosuwindam@gmail.com>
2024-12-03 11:28:28 +0900

    run config and trace

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"epaperifdb/config"
 	"epaperifdb/controller"
 	"os"
 	"os/signal"
@@ -9,6 +10,9 @@ import (
 )
 
 func init() {
+	if err := config.Init(); err != nil {
+		panic(err)
+	}
 	if err := controller.Init(); err != nil {
 		panic(err)
 	}
@@ -18,6 +22,10 @@ func main() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	ctx, cancel := context.WithCancel(context.Background())
+	tshutdown, terr := config.TracerStart(config.TraData.GrpcURL, config.TraData.ServiceName, ctx)
+	if terr != nil {
+		defer tshutdown(context.Background())
+	}
 	defer cancel()
 	go func(ctx context.Context) {
 		if err := controller.Run(ctx); err != nil {
