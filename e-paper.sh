cdb71d94249175d980b048ff366d9c81c8ab88c0
karosuwindam <karosuwindam@gmail.com>
2025-01-09 17:10:17 +0900

    update sh

--- e-paper.sh
+++ e-paper.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 cd /home/pi/e-paper-inflaxdb/main
+export V2_ROTATE_180=false
+export INIT_CLEAR_FLAG=false
+export V2_IMG_MIRROR=true
+export V2_FLAG=true
+export TRASH_FLAG=true
 ./epaperifdb
